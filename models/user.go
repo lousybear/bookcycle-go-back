5258b9c9e319945893583384a62ce2c117ecb5dc
lousybear <ashishkrse@gmail.com>
2025-08-03 12:53:15 +0530

    updated user model

--- models/user.go
+++ models/user.go
@@ -4,6 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name     string             `bson:"name" json:"name"`
 	Username string             `bson:"username" json:"username"`
 	Email    string             `bson:"email" json:"email"`
 	Phone    string             `bson:"phone" json:"phone"`
